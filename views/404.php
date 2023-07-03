77420d0d0a962989286f0498e7acb24d771375a9
Go3shom <shadi.muhammad001@gmail.com>
2023-07-03 16:08:06 +0300

    Episode_15: Adding 404 View FIle

---
+++ views/404.php
@@ -0,0 +1,14 @@
+<?php require 'partials/nav.php'; ?>
+<?php require 'partials/header.php'; ?>
+
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <h1 class="text-2xl font-bold">Sorry, Page Not Found</h1>
+
+        <p class="mt-4">
+            <a class="text-blue-500 undeline" href="/">Back to Home</a>
+        </p>
+    </div>
+</main>
+
+<?php require 'partials/footer.php'; ?>
