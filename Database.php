ba8151066714b8185436e55966f2ce3bd15a1e1b
Go3shom <shadi.muhammad001@gmail.com>
2023-07-06 20:44:09 +0300

    Episode_19: Enhancing 'Database' Class dsn Connection String To Be More Generic

--- Database.php
+++ Database.php
@@ -7,7 +7,14 @@ class Database
 
     public function __construct()
     {
-        $dsn = "mysql:host=localhost;dbname=laracasts_php;port=3306;charset=utf8mb4";
+        $config = [
+            'host' => 'localhost',
+            'dbname' => 'laracasts_php',
+            'port' => '3306',
+            'charset' => 'utf8mb4',
+        ];
+
+        $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};port={$config['port']};charset={$config['charset']}";
 
         $this->connection = new PDO($dsn, 'root', 'password@1234', [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
