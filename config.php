47ac40de59567b271e37d400691c50294ac49b7a
Go3shom <shadi.muhammad001@gmail.com>
2023-07-06 21:23:34 +0300

    Episode_19: Make A Key Value Pair For 'database' Array

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
 <?php
 
 return [
-    'host' => 'localhost',
-    'dbname' => 'laracasts_php',
-    'port' => '3306',
-    'charset' => 'utf8mb4',
+    'database' => [
+        'host' => 'localhost',
+        'dbname' => 'laracasts_php',
+        'port' => '3306',
+        'charset' => 'utf8mb4',
+    ],
 ];
