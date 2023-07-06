de0c470d36a9a79eb0865821e42740b0b1ea3963
Go3shom <shadi.muhammad001@gmail.com>
2023-07-06 21:24:49 +0300

    Episode_19: Change The 'Database' Instanse To Accept 'config' Array Instead Of String

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require 'Database.php';
 
 $config = require('config.php');
 
-$db = new Database($config);
+$db = new Database($config['database']);
 
 $post = $db->query("SELECT * FROM `posts` WHERE `id` = 1;")->fetch();
 
