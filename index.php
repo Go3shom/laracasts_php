1c59c85e4846b6235c5ab50a6ef608f11b989c30
Go3shom <shadi.muhammad001@gmail.com>
2023-07-06 14:04:45 +0300

    Episod_18: Making A Dedecated 'Database' Class With 'query' Method

--- index.php
+++ index.php
@@ -5,13 +5,22 @@ require 'functions.php';
 
 
 // Connect to our MySQL database.
-$dns = "mysql:host=localhost;dbname=laracasts_php;port=3306;user=root;password=password@1234;charset=utf8mb4";
-$pdo = new PDO($dns);
+class Database
+{
+    public function query()
+    {
+        $dns = "mysql:host=localhost;dbname=laracasts_php;port=3306;user=root;password=password@1234;charset=utf8mb4";
+        $pdo = new PDO($dns);
 
-$statement = $pdo->prepare("SELECT * FROM `posts`;");
-$statement->execute();
+        $statement = $pdo->prepare("SELECT * FROM `posts`;");
+        $statement->execute();
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+$db = new Database();
+$posts = $db->query();
 
 
 foreach ($posts as $post) {
