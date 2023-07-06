0d212495e4e7cbefc9d0b4a528bdf0064297550a
Go3shom <shadi.muhammad001@gmail.com>
2023-07-06 13:42:39 +0300

    Episod_17: Connecting To MySQL Database

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+
+// Connect to our MySQL database.
+$dns = "mysql:host=localhost;dbname=laracasts_php;port=3306;user=root;password=password@1234;charset=utf8mb4";
+$pdo = new PDO($dns);
+
+$statement = $pdo->prepare("SELECT * FROM `posts`;");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+foreach ($posts as $post) {
+    echo '<li>' . $post['title'] . '</li>';
+}
