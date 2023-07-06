95765da9b850e31f3beca02e64c8e576d226532c
Go3shom <shadi.muhammad001@gmail.com>
2023-07-06 15:09:33 +0300

    Episode_18: Make A Dedecated File For 'Database' Class To Put The Logic In Instead Of 'index.php'

--- index.php
+++ index.php
@@ -1,33 +1,11 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
 // require 'router.php';
 
 
-// Connect to our MySQL database.
-class Database
-{
-    public $connection;
-
-
-    public function __construct()
-    {
-        $dsn = "mysql:host=localhost;dbname=laracasts_php;port=3306;user=root;password=password@1234;charset=utf8mb4";
-
-        $this->connection = new PDO($dsn);
-    }
-
-    public function query($query)
-    {
-        $statement = $this->connection->prepare($query);
-        $statement->execute();
-
-        return $statement;
-    }
-}
-
 $db = new Database();
-// Using `fetch` instead, will return a single post perfectly fine
 $post = $db->query("SELECT * FROM `posts` WHERE `id` = 1;")->fetch(PDO::FETCH_ASSOC);
 
 
