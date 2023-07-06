359e056f3c7d217dce6475c95205815e51222cb8
Go3shom <shadi.muhammad001@gmail.com>
2023-07-06 20:51:25 +0300

    Episode_19: Enhancing 'Database' Class dsn Connection String Configuration Again

--- Database.php
+++ Database.php
@@ -14,7 +14,8 @@ class Database
             'charset' => 'utf8mb4',
         ];
 
-        $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};port={$config['port']};charset={$config['charset']}";
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
+
 
         $this->connection = new PDO($dsn, 'root', 'password@1234', [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
