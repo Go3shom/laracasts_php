f32884c9834588e755063f816d16defe4d22ceeb
Go3shom <shadi.muhammad001@gmail.com>
2023-07-08 17:05:15 +0300

    Episod_22: Creating Notes Dedecated Controller & View Files

---
+++ controllers/notes.php
@@ -0,0 +1,15 @@
+<?php
+
+$config = require('config.php');
+$db = new Database($config['database']);
+
+
+$heading = 'Notes';
+
+
+$notes = $db->query("SELECT * FROM `notes`")->fetchAll();
+
+
+// dd($notes);
+
+require 'views/notes.view.php';
