366de84738465fea3c1e260a9f3ab5d310c889a0
Go3shom <shadi.muhammad001@gmail.com>
2023-07-10 20:11:16 +0300

    Episode_28: Calling Validation's 'string' Method As 'static' Instead Of Initializing New Object In Ervey time

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -15,10 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $errors = [];
 
 
-    $validator = new Validator();
-
-
-    if (!$validator->string($_POST['body'], 1, 1_000)) {
+    if (!Validator::string($_POST['body'], 1, 1_000)) {
         $errors['body'] = 'A body of no more than 1,000 characters is required.';
     }
 
