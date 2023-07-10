7f4670b628df99be90e41f5e81d9f091e4807e2b
Go3shom <shadi.muhammad001@gmail.com>
2023-07-10 20:02:05 +0300

    Episode_28: Quick Fix For Error Message Showing

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -17,13 +17,13 @@
                                 <div class="mt-2">
                                     <textarea id="body" name="body" rows="5" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Write your note here…"><?= $_POST['body'] ?? '' ?></textarea>
                                 </div>
-                            </div>
 
-                            <?php if (isset($errors['body'])) : ?>
-                                <p class="text-red-500 text-xs mt-2">
-                                    <?= $errors['body'] ?>
-                                </p>
-                            <?php endif; ?>
+                                <?php if (isset($errors['body'])) : ?>
+                                    <p class="text-red-500 text-xs mt-2">
+                                        <?= $errors['body'] ?>
+                                    </p>
+                                <?php endif; ?>
+                            </div>
 
 
                             <div class="lg:col-span-3">
