9d3c095069c0daf4ca0b4ef06530791277bdbfa3
Go3shom <shadi.muhammad001@gmail.com>
2023-07-10 19:59:27 +0300

    Episode_28: Updating Validator's 'string' Method To Accept Min & Max Values

--- Validator.php
+++ Validator.php
@@ -3,8 +3,11 @@
 
 class Validator
 {
-    public function string($value)
+    public function string($value, $min = 1, $max = INF)
     {
-        return strlen(trim($value)) === 0;
+        $value = trim($value);
+
+
+        return strlen($value) >= $min && strlen($value) <= $max;
     }
 }
