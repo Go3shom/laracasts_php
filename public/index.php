b6c04edd7ec21e6099ff7c573a883dcc43f7272f
Go3shom <shadi.muhammad001@gmail.com>
2023-07-11 14:29:01 +0300

    Episode_30: Encapsulate 'BASE_PATH' Constant Into Its Own Helper Function 'base_path()' To Enhance The Code

--- public/index.php
+++ public/index.php
@@ -3,6 +3,6 @@
 const BASE_PATH = __DIR__ . '/../';
 
 require BASE_PATH . 'functions.php';
-require BASE_PATH . 'Database.php';
-require BASE_PATH . 'Response.php';
-require BASE_PATH . 'router.php';
+require base_path('Database.php');
+require base_path('Response.php');
+require base_path('router.php');
