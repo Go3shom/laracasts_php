457696d693628e058bf2af35b8925e5b6eb2ed76
Go3shom <shadi.muhammad001@gmail.com>
2023-07-11 14:22:48 +0300

    Episode_30: Ading 'BASE_PATH' Constant For The Whole Project To Ease Requiring Files

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 
-require 'functions.php';
-require 'Database.php';
-require 'Response.php';
-require 'router.php';
+const BASE_PATH = __DIR__ . '/../';
+
+require BASE_PATH . 'functions.php';
+require BASE_PATH . 'Database.php';
+require BASE_PATH . 'Response.php';
+require BASE_PATH . 'router.php';
