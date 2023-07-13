e0739e28ceaa4ceb8298a1b2b35d7c23d704208e
Go3shom <shadi.muhammad001@gmail.com>
2023-07-13 20:55:51 +0300

    Episode_31: Substitute '/' With 'DIRECTORY_SEPARATOR' For Using Separator Depending On Operating System

--- public/index.php
+++ public/index.php
@@ -7,11 +7,7 @@ require BASE_PATH . 'Core/functions.php';
 
 spl_autoload_register(function ($class) {
 
-    // dd($class); // => Core\Database
-
-    $class = str_replace('\\', '/', $class);
-
-    // dd($class);  // => Core/Database
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
 
     require base_path("Core/{$class}.php");
 });
