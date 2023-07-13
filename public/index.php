2b13088fed68d68be2157b3f7cd2c403922d0ca3
Go3shom <shadi.muhammad001@gmail.com>
2023-07-13 20:51:50 +0300

    Episode_31: Using 'str_replace()' Built In Function To Replace '\' With '/' When Autoloading Classes

--- public/index.php
+++ public/index.php
@@ -6,6 +6,13 @@ require BASE_PATH . 'Core/functions.php';
 
 
 spl_autoload_register(function ($class) {
+
+    // dd($class); // => Core\Database
+
+    $class = str_replace('\\', '/', $class);
+
+    // dd($class);  // => Core/Database
+
     require base_path("Core/{$class}.php");
 });
 
