5298ad7e84497f8afb1e449310b8ffd84215f198
Go3shom <shadi.muhammad001@gmail.com>
2023-07-13 20:57:48 +0300

    Episode_31: Removing 'Core' From Autolading Registered Classes

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ spl_autoload_register(function ($class) {
 
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
 
-    require base_path("Core/{$class}.php");
+    require base_path("{$class}.php");
 });
 
 
