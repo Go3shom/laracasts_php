fbc630e052054ed3f8a544b1cbb4848fa2245a38
Go3shom <shadi.muhammad001@gmail.com>
2023-07-18 17:21:56 +0300

    Episode_32: Adding Form For Commiting A 'delete' Action On The 'note'

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -10,6 +10,15 @@
         <p>
             <?= htmlspecialchars($note['body']) ?>
         </p>
+
+
+        <form class="my-5" method="POST">
+            <input type="hidden" name="id" value="<?= $note['id'] ?>">
+
+            <button type="submit" class="rounded-lg px-3 py-2 text-sm font-semibold text-white shadow-sm bg-red-900 hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">
+                Delete
+            </button>
+        </form>
     </div>
 </main>
 
