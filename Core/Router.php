dc3015bd3fc21c718374c9554cac38bba7f2f77b
Go3shom <shadi.muhammad001@gmail.com>
2023-07-19 16:15:39 +0300

    Episod_33: Updating Rrouter Class, & View To Accept HTTP Method Spoofing

--- Core/Router.php
+++ Core/Router.php
@@ -52,23 +52,24 @@ class Router
             'method' => 'DELETE',
         ];
     }
+
+    public function route($uri, $method)
+    {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+                return require base_path($route['controller']);
+            }
+        }
+
+        $this->abort();
+    }
+
+    protected function abort($code = 404)
+    {
+        http_response_code($code);
+
+        require base_path("views/{$code}.php");
+
+        die();
+    }
 }
-
-
-// function routeToController($uri, $routes)
-// {
-//     if (array_key_exists($uri, $routes)) {
-//         require base_path($routes[$uri]);
-//     } else {
-    //         abort();
-    //     }
-    // }
-
-// protected function abort($code = 404)
-// {
-//     http_response_code($code);
-
-//     require base_path("views/{$code}.php");
-
-//     die();
-// }
