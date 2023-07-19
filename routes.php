32b488913ca96394a6638e6e89cb0d5b783d3df3
Go3shom <shadi.muhammad001@gmail.com>
2023-07-19 16:18:15 +0300

    Episod_33: Updating Routes To Accept Rrouter Instance With Readable Methods

--- routes.php
+++ routes.php
@@ -1,23 +1,9 @@
 <?php
 
-// return [
-//     '/' => 'controllers/index.php',
-//     '/about' => 'controllers/about.php',
-//     '/notes' => 'controllers/notes/index.php',
-//     '/note' => 'controllers/notes/show.php',
-//     '/notes/create' => 'controllers/notes/create.php',
-//     '/contact' => 'controllers/contact.php',
-// ];
+$router->get('/', 'controllers/index.php');
+$router->get('/about', 'controllers/about.php');
+$router->get('/contact', 'controllers/contact.php');
 
-return [
-    [
-        'uri' => '/',
-        'controller' => 'controllers/index.php',
-        'method' => 'GET'
-    ],
-    [
-        'uri' => '/',
-        'controller' => 'controllers/index.php',
-        'method' => 'POST'
-    ],
-];
+$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/note', 'controllers/notes/show.php');
+$router->get('/notes/create', 'controllers/notes/create.php');
